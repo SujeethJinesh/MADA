6f74d24037a74f454a5e6a6744f0b831e238f230
SujeethJinesh <sujeethjinesh@gmail.com>
2017-12-18 14:26:49 -0800

    Move and update files

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+template_dir = os.path.abspath('front_end/src/')
+app = Flask(__name__, template_folder=template_dir)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('templates/index.html')
 
 
 @app.route('/hello')
 def hello():
-    return render_template('hello.html')
+    return render_template('templates/hello.html')
 
 
 if __name__ == '__main__':
