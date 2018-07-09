92f6c4094787daa4f784a39ece13d8b3eb1da31e
SujeethJinesh <sujeethjinesh@gmail.com>
2018-07-08 20:41:01 -0700

    Fix

---
+++ back_end/data_processing/Sentdex.py
@@ -0,0 +1,6 @@
+import pandas as pd
+pd.core.common.is_list_like = pd.api.types.is_list_like
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# TODO: figure out deployment procedure.
